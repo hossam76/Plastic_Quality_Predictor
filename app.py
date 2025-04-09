37d3c849269fd6dc0192328bb12facd65a212f5d
MD RAKIB HOSSAIN <117323310+hossam76@users.noreply.github.com>
2025-04-09 17:19:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import joblib
 import io
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay
